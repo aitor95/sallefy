a201317de512a43dc133ec830648fefbf329ee7a
miquelsaula <miquelsaula@gmail.com>
2020-04-05 13:27:06 +0200

    #10

--- app/src/main/java/com/salle/android/sallefy/controller/fragments/MeSongFragment.java
+++ app/src/main/java/com/salle/android/sallefy/controller/fragments/MeSongFragment.java
@@ -8,17 +8,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.salle.android.sallefy.R;
+import com.salle.android.sallefy.controller.adapters.TrackListAdapter;
+import com.salle.android.sallefy.controller.callbacks.TrackListCallback;
+import com.salle.android.sallefy.controller.music.MusicCallback;
+import com.salle.android.sallefy.controller.restapi.callback.TrackCallback;
+import com.salle.android.sallefy.controller.restapi.manager.TrackManager;
+import com.salle.android.sallefy.model.Track;
 
-public class MeSongFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeSongFragment extends Fragment implements MusicCallback, TrackListCallback, TrackCallback {
 
 	public static final String TAG = MeSongFragment.class.getName();
 
 	private RecyclerView mRecyclerView;
-
-
+	private ArrayList<Track> mTracks;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,13 +39,68 @@ public class MeSongFragment extends Fragment {
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_me_song, container, false);
 		initViews(v);
+		getData();
 		return v;
 	}
 
 	private void initViews(View v) {
 		mRecyclerView = (RecyclerView) v.findViewById(R.id.dynamic_recyclerView);
+		LinearLayoutManager manager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+		TrackListAdapter adapter = new TrackListAdapter(this, getActivity(), null);
+		mRecyclerView.setLayoutManager(manager);
+		mRecyclerView.setAdapter(adapter);
+	}
 
+	private void getData() {
+		//TODO: cambiar por cojer lascanciones que tocan
+		TrackManager.getInstance(getActivity()).getAllTracks(this);
+		mTracks = new ArrayList<>();
+	}
 
+	@Override
+	public void onTracksReceived(List<Track> tracks) {
+		mTracks = (ArrayList) tracks;
+		TrackListAdapter adapter = new TrackListAdapter(this, getActivity(), mTracks);
+		mRecyclerView.setAdapter(adapter);
+	}
+
+	@Override
+	public void onTrackSelected(Track track) {
+
+	}
+
+	@Override
+	public void onTrackSelected(int index) {
+
+	}
+
+	@Override
+	public void onMusicPlayerPrepared() {
+
+	}
+
+	@Override
+	public void onNoTracks(Throwable throwable) {
+
+	}
+
+	@Override
+	public void onPersonalTracksReceived(List<Track> tracks) {
+
+	}
+
+	@Override
+	public void onUserTracksReceived(List<Track> tracks) {
+
+	}
+
+	@Override
+	public void onCreateTrack() {
+
+	}
+
+	@Override
+	public void onFailure(Throwable throwable) {
 
 	}
 }

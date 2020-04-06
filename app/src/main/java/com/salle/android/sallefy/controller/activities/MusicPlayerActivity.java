dd9d225c2beb7147937f1af7e98310eda5bef96e
HobJob <adria.arroyo@outlook.es>
2020-04-06 17:58:55 +0200

    Finalizado diseño xml #90

---
+++ app/src/main/java/com/salle/android/sallefy/controller/activities/MusicPlayerActivity.java
@@ -0,0 +1,16 @@
+package com.salle.android.sallefy.controller.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.salle.android.sallefy.R;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}

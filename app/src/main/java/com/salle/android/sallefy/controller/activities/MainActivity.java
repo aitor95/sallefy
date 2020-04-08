10aabf6937daf0642fed1c0f0ab9a137551ef3e4
Aitor Blesa <aitor.blesa@students.salle.url.edu>
2020-04-08 19:21:19 +0200

    Cambios visuales Vista ajustes. #94

--- app/src/main/java/com/salle/android/sallefy/controller/activities/MainActivity.java
+++ app/src/main/java/com/salle/android/sallefy/controller/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.salle.android.sallefy.controller.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.session.MediaController;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/salle/android/sallefy/controller/activities/SettingsActivity.java
+++ app/src/main/java/com/salle/android/sallefy/controller/activities/SettingsActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -15,7 +14,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
 
         RelativeLayout optionStats = findViewById(R.id.settings_option_stats);
         optionStats.setOnClickListener(view -> {

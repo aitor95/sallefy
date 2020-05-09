82d9063fbc21e197b42f4fca8dfa94514d6fe124
Miriam Lopez Nieto <miriam.lopez@students.salle.url.edu>
2020-05-09 20:25:59 +0200

    #157 - Arreglar Social por paginación to test

--- app/src/main/java/com/salle/android/sallefy/controller/restapi/service/TrackService.java
+++ app/src/main/java/com/salle/android/sallefy/controller/restapi/service/TrackService.java
@@ -22,7 +22,7 @@ public interface TrackService {
     Call<List<Track>> getAllTracks(@Header("Authorization") String token);
 
     @GET("tracks")
-    Call<List<Track>> getAllTracksPagination(@Header("Authorization") String token, @Query("page") int page, @Query("size") int size);
+    Call<List<Track>> getAllTracksPagination(@Header("Authorization") String token, @Query("page") int page, @Query("size") int size, @Query("recent") boolean recent);
 
     @GET("tracks")
     Call<List<Track>> getAllTracksSocial(@Header("Authorization") String token, @Query("size") int size, @Query("recent") boolean recent);
